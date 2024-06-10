84a6966924a63a28ffe64e75b0709b01e93143c8
hilal <hilal123anwar@gmail.com>
2024-06-10 11:08:22 +0530

    Added new question

---
+++ src/project1/Loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(int argc, char const *argv[]) 
+{
+    int c = 1;
+    while (c <= 6)
+    {
+        if (c <= 3)
+        {
+            cout << "Faisal\t" << "Saif" << endl;
+        }
+        else
+        {
+            cout << "They love to code\n";
+        }
+        c = c + 1;
+    }
+
+    cout << "The loop has ended\n";
+    return 0;
+}
