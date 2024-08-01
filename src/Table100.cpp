799f177162d33106bd6197c870189faa838e6b3e
Hilal-Anwar <hilal123anwar@gmail.com>
2024-08-01 12:12:23 +0530

    Added new questions

---
+++ src/Table100.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for (int i = 1; i <= 1000; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << i << " X " << j << " = " << i * j << endl;
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
